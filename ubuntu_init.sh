1f80f4a44bf49f148ee68582a84dacf6a54ac574
gangbo <daigangbo@gmail.com>
2015-11-01 20:39:42 +0800

    tmux.conf

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -8,6 +8,8 @@ mkdir -p ~/mygit/
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/gangbo/vim_open_pm.git ~/mygit/vim_open_pm
 ln -sf ~/mygit/vim_open_pm/vimrc ~/.vimrc
+ln -sf ~/mygit/vim_open_pm/.tmux.conf ~/.tmux.conf
+touch ~/.tmux.conf.local
 vim +PluginInstall +qall
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
